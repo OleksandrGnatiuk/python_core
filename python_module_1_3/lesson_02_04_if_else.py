8111ea91fdba543fa9c6f02ef822881e31685145
Oleksandr.Gnatiuk <oleksandr.gnatiuk@gmail.com>
2022-10-30 17:33:22 +0200

    Add files

---
+++ python_module_1_3/lesson_02_04_if_else.py
@@ -0,0 +1,24 @@
+# Після перевірок змінна result повинна містити одне з чотирьох значень:
+#     "Positive even number"
+#     "Positive odd number"
+#     "Negative number"
+#     "It is zero"
+
+# Підказка: перевірка на парність виконується порівнянням залишку від поділу на 2 з 0 або 1.
+# Нагадаємо, залишок від ділення можна отримати після операції %
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    if num % 2 == 1:
+        result = "Positive odd number"
+    else:
+        result = "Positive even number"
+
+elif num < 0:
+    result = "Negative number"
+
+else:
+    result = "It is zero"
+
+print(result)
