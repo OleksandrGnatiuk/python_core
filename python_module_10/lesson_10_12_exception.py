b36175d73ce465b03ba509b8044d1cfc3dfcc4e2
Oleksandr.Gnatiuk <oleksandr.gnatiuk@gmail.com>
2022-11-23 17:58:41 +0200

    "Add files"

---
+++ python_module_10/lesson_10_12_exception.py
@@ -0,0 +1,22 @@
+class IDException(Exception):
+    pass
+
+
+def add_id(id_list, employee_id):
+    if employee_id[:2] == "01":
+        id_list.append(employee_id)
+        return id_list
+    else:
+        raise IDException
+
+
+
+# Створіть клас IDException, який успадковуватиме клас Exception.
+
+# Також реалізуйте функцію add_id(id_list, employee_id), яка додає до 
+# списку id_list ідентифікатор користувача employee_id та повертає вказаний 
+# оновлений список id_list.
+
+# Функція add_id буде викликати власне виключення IDException, 
+# якщо employee_id не починається з '01', інакше employee_id буде 
+# додано до списку id_list.
