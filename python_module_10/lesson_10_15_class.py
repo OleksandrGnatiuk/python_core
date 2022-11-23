1f7402a78f22dbab723f63b21402dd47bad443db
Oleksandr.Gnatiuk <oleksandr.gnatiuk@gmail.com>
2022-11-23 19:26:17 +0200

    "Add files"

---
+++ python_module_10/lesson_10_15_class.py
@@ -0,0 +1,40 @@
+class Contacts:
+    current_id = 1
+
+    def __init__(self):
+        self.contacts = []
+
+    def list_contacts(self):
+        return self.contacts
+
+    def add_contacts(self, name, phone, email, favorite):
+        self.contacts.append(
+            {
+                "id": Contacts.current_id,
+                "name": name,
+                "phone": phone,
+                "email": email,
+                "favorite": favorite,
+            }
+        )
+        Contacts.current_id += 1
+
+    def get_contact_by_id(self, id):
+        for contact in self.contacts:
+            if id == contact["id"]:
+                return contact
+        
+phonebook = Contacts()
+phonebook.add_contacts('Wylie Pope', '(692) 802-2949', 'est@utquamvel.net', True)
+phonebook.add_contacts('Cyrus Jackson', '(501) 472-5218', 'nibh@semsempererat.com', False)
+print(phonebook.get_contact_by_id(2))
+      
+
+
+# Продовжуємо розширювати функціональність класу Contacts. 
+# На цьому етапі ми додамо до класу метод get_contact_by_id. 
+# Метод повинен шукати контакт по унікальному id у списку contacts 
+# та повертати словник з нього із зазначеним ключем id. 
+# Якщо словника із зазначеним id у списку contacts не знайдено, метод повертає None.
+
+# Примітка: для правильного проходження тесту не створюйте екземпляр класу в коді.     
